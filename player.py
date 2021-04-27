90d90c041795346b93005b95e471b35c611327a8
Zilosz <milosz.fecko@hotmail.com>
2021-04-27 18:20:23 -0400

    spacing

--- player.py
+++ player.py
@@ -1,19 +1,12 @@
 from user import User
 
 
-
 class Player(User):
     
-
-    CHOOSER = 'Player'
-
+    CHOOSER_NAME = 'Player'
 
     def __init__(self, game, color, win_color):
-
         User.__init__(self, game, color, win_color)
 
-
     def initiate_turn(self):
-
         self.game.enable_player_input()
-
